ad82662f5c09bddd93eea80ae1dd0a9e2967df34
arijitbasu79 <cs.arijitbasu@gmail.com>
2021-07-18 02:11:14 +0530

    Initial Commit

---
+++ microservices-demo/twitter-to-kafka-service/src/main/java/com/github/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.github.microservices.demo.twitter.to.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+		System.out.println("Main...");
+
+	}
+
+}
