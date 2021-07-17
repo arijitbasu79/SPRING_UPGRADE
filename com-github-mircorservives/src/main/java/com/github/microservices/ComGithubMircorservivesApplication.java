e45960bc4bc1894661968ca5af6d55f073a5c34a
arijitbasu79 <cs.arijitbasu@gmail.com>
2021-07-18 00:45:41 +0530

    initial commit

---
+++ com-github-mircorservives/src/main/java/com/github/microservices/ComGithubMircorservivesApplication.java
@@ -0,0 +1,13 @@
+package com.github.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComGithubMircorservivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComGithubMircorservivesApplication.class, args);
+	}
+
+}
